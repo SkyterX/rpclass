c819c9a1578b9a54473287962a095faaa1188c4d
msamoylenko <ghost1212@mail.ru>
2015-10-22 00:27:26 +0500

    Template PropertyMaps.

---
+++ src/graph/include/graph/StaticGraphPropertyMaps.hpp
@@ -0,0 +1,79 @@
+#pragma once
+
+#include "graph.hpp"
+
+// VertexPropertyMap
+namespace graph {
+	template <typename VertexProperties, typename Graph>
+	class VertexPropertyMap {
+	private:
+		Graph& graph;
+	public:
+		explicit VertexPropertyMap(Graph& graph)
+			: graph(graph) {}
+
+		using key_type = typename graph_traits<Graph>::vertex_descriptor;
+		using value_type = VertexProperties;
+		using reference = value_type&;
+		using category = boost::read_write_property_map_tag;
+
+		reference get(const key_type& key) const {
+			return graph.vertices[key].properties;
+		}
+
+		void put(const key_type& key, const value_type& value) const {
+			graph.vertices[key].properties = value;
+		}
+	};
+
+	template <typename VertexProperties, typename Graph>
+	inline typename VertexPropertyMap<VertexProperties, Graph>::reference get(
+		const VertexPropertyMap<VertexProperties, Graph>& pm,
+		const typename VertexPropertyMap<VertexProperties, Graph>::key_type& key) {
+		return pm.get(key);
+	}
+
+	template <typename VertexProperties, typename Graph>
+	inline void put(
+		VertexPropertyMap<VertexProperties, Graph>& pm,
+		const typename VertexPropertyMap<VertexProperties, Graph>::key_type& key,
+		const typename VertexPropertyMap<VertexProperties, Graph>::reference value) {
+		pm.put(key, value);
+	}
+
+}
+
+// EdgePropertyMap
+namespace graph {
+	template <typename EdgeProperties, typename Graph>
+	class EdgePropertyMap {
+	public:
+		using key_type = typename graph_traits<Graph>::edge_descriptor;
+		using value_type = EdgeProperties;
+		using reference = value_type&;
+		using category = boost::read_write_property_map_tag;
+
+		value_type& get(const key_type& key) const {
+			return *key.properties;
+		}
+
+		void put(const key_type& key, const value_type& value) const {
+			*key.properties = value;
+		}
+	};
+
+	template <typename EdgeProperties, typename Graph>
+	inline typename EdgePropertyMap<EdgeProperties, Graph>::value_type& get(
+		const EdgePropertyMap<EdgeProperties, Graph>& pm,
+		const typename EdgePropertyMap<EdgeProperties, Graph>::key_type& key) {
+		return pm.get(key);
+	}
+
+	template <typename EdgeProperties, typename Graph>
+	inline void put(
+		EdgePropertyMap<EdgeProperties, Graph>& pm,
+		const typename EdgePropertyMap<EdgeProperties, Graph>::key_type& key,
+		const typename EdgePropertyMap<EdgeProperties, Graph>::value_type& value) {
+		pm.put(key, value);
+	}
+}
--- src/graph/include/graph/static_graph.hpp
+++ src/graph/include/graph/static_graph.hpp
@@ -9,22 +9,24 @@
 #include "util/Collection.hpp"
 #include "StaticGraphIterators.hpp"
 #include "BasicGraphStructures.hpp"
+#include "StaticGraphPropertyMaps.hpp"
+#include "properties.hpp"
 
-namespace graph
-{
+namespace graph {
 	struct StaticGraphTraversalCategory :
 			public boost::bidirectional_graph_tag,
 			public boost::vertex_list_graph_tag { };
-	
-	struct vertex_properties_stub {};
 
+	struct vertex_properties_stub {};
 	struct edge_properties_stub {};
-	
 
 
 	class StaticGraph {
 		class VertexIterator;
+		using VertexProperties = vertex_properties_stub;
+		using EdgeProperties = edge_properties_stub;
 	public:
+		using type = StaticGraph;
 
 		using edge_size_type = uint32_t;
 		using vertices_size_type = uint32_t;
@@ -35,19 +37,19 @@ namespace graph
 		using edge_parallel_category = boost::disallow_parallel_edge_tag;
 		using traversal_category = StaticGraphTraversalCategory;
 
-		using EdgeType = FancyEdge<vertex_descriptor, edge_properties_stub>; 
+		using EdgeType = FancyEdge<vertex_descriptor, EdgeProperties>;
 		using edge_descriptor = FancyEdgeDescriptor<EdgeType::VertexType, EdgeType::EdgePropertiesType>;
 
 	private:
 		using AdjacenciesVecType = std::vector<EdgeType*>;
 		using AdjacenciesVecIteratorType = AdjacenciesVecType::const_iterator;
-		
+
 	public:
 		using edge_iterator = EdgeIterator<AdjacenciesVecIteratorType, edge_descriptor>;
 		using adjacency_iterator = AdjacencyIterator<AdjacenciesVecIteratorType, vertex_descriptor>;
 		using out_edge_iterator = edge_iterator;
 		using in_edge_iterator = edge_iterator;
-		using VertexType = Vertex<AdjacenciesVecIteratorType, vertex_properties_stub>;
+		using VertexType = Vertex<AdjacenciesVecIteratorType, VertexProperties>;
 
 	private:
 		using VerticesVecType = std::vector<VertexType>;
@@ -60,8 +62,9 @@ namespace graph
 		class VertexCollection;
 		class AdjacencyCollection;
 		class EdgeCollection;
-		class VertexPropertyMap;
-		class EdgePropertyMap;
+		using VertexPropertyMapType = VertexPropertyMap<VertexProperties, type>;
+		using EdgePropertyMapType = EdgePropertyMap<EdgeProperties, type>;
+		friend class VertexPropertyMapType;
 
 		
 
@@ -69,7 +72,7 @@ namespace graph
 
 		template <class PairIterator>
 		StaticGraph(PairIterator begin, PairIterator end,
-		            vertices_size_type n, edges_size_type m = 0);
+					vertices_size_type n, edges_size_type m = 0);
 
 		const VertexCollection& Vertices() const;
 		AdjacencyCollection OutAdjacencies(const vertex_descriptor& v) const;
@@ -78,8 +81,8 @@ namespace graph
 		EdgeCollection InEdges(const vertex_descriptor& v) const;
 		edges_size_type EdgesCount() const;
 
-		const EdgePropertyMap& GetEdgePropertyMap() const;
-		const VertexPropertyMap& GetVertexPropertyMap() const;
+		const EdgePropertyMapType& GetEdgePropertyMap() const;
+		const VertexPropertyMapType& GetVertexPropertyMap() const;
 	private:
 		void Initialize();
 
@@ -87,8 +90,8 @@ namespace graph
 		VerticesVecType vertices;
 		EdgesVecType edges;
 		AdjacenciesSeparatorsVecType edgesSeparators;
-		std::unique_ptr<EdgePropertyMap> edgePropertyMap;
-		std::unique_ptr<VertexPropertyMap> vertexPropertyMap;
+		std::unique_ptr<EdgePropertyMapType> edgePropertyMap;
+		std::unique_ptr<VertexPropertyMapType> vertexPropertyMap;
 		std::unique_ptr<VertexCollection> vertexCollection;
 	};
 
@@ -143,14 +146,41 @@ namespace graph
 	};
 }
 
-#include "StaticGraphPropertyMaps.h"
+// PropertyMaps
+namespace graph {
+
+	template <>
+	struct property_map<StaticGraph, vertex_bundle_t> {
+		using type = StaticGraph::VertexPropertyMapType;
+	};
+
+	template <>
+	struct property_map<StaticGraph, edge_bundle_t> {
+		using type = StaticGraph::EdgePropertyMapType;
+	};
+
+	template <>
+	inline property_map<StaticGraph, vertex_bundle_t>::type
+	get<StaticGraph>(const vertex_bundle_t&, StaticGraph& graph) {
+		return graph.GetVertexPropertyMap();
+	}
+
+	template <>
+	inline property_map<StaticGraph, edge_bundle_t>::type
+	get<StaticGraph>(const edge_bundle_t&, StaticGraph& graph) {
+		return graph.GetEdgePropertyMap();
+	}
+}
+
 #include "StaticGraphTools.hpp"
 #include "StaticGraphBuilder.hpp"
 
 // StaticGraph
-namespace graph
-{
-	inline StaticGraph::StaticGraph() : vertexCollection(nullptr), edgePropertyMap(), vertexPropertyMap(new VertexPropertyMap(*this)) {}
+namespace graph {
+	inline StaticGraph::StaticGraph()
+		: edgePropertyMap(),
+		  vertexPropertyMap(new VertexPropertyMapType(*this)),
+		  vertexCollection(nullptr) {}
 
 	template <class PairIterator>
 	StaticGraph::StaticGraph(PairIterator begin, PairIterator end, vertices_size_type n, edges_size_type m) : StaticGraph() {
@@ -197,11 +227,11 @@ namespace graph
 		this->vertexCollection = std::unique_ptr<VertexCollection>(new VertexCollection(*this));
 	}
 
-	inline const StaticGraph::VertexPropertyMap& StaticGraph::GetVertexPropertyMap() const {
+	inline const StaticGraph::VertexPropertyMapType& StaticGraph::GetVertexPropertyMap() const {
 		return *vertexPropertyMap;
 	}
 
-	inline const StaticGraph::EdgePropertyMap& StaticGraph::GetEdgePropertyMap() const {
+	inline const StaticGraph::EdgePropertyMapType& StaticGraph::GetEdgePropertyMap() const {
 		return *edgePropertyMap;
 	}
 
