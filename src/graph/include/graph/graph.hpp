6f2d8d2a1d0e45ec1b286b51b265ee33357000e4
Vitaly Osipov <developer@osipov.mobi>
2015-10-24 16:03:36 +0500

    Adressing issue #2, generating default graph_traits depending on graph traversal category tag. Adressing issue #3, moving edges_size_type to the graph concept (different from boost). edges_size_type is part of edge list graph concept, but we do not need to support this concept.

--- src/graph/include/graph/graph.hpp
+++ src/graph/include/graph/graph.hpp
@@ -2,11 +2,7 @@
 #include <type_traits>
 namespace graph{
 
-
-
-
 // traversal category tags
-
 struct incidence_graph_tag { };
 struct adjacency_graph_tag { };
 struct bidirectional_graph_tag : virtual incidence_graph_tag { };
@@ -35,42 +31,84 @@ public:
     static const bool value = type::value;
 };
 
-};
-
-//default graph traits specializations depending on traversal category of the graph
-
-// default graph traits
-
-template <typename Graph, typename TagDispatcher = typename Graph::traversal_category>
-struct graph_traits {
-    using vertex_descriptor      = typename Graph::vertex_descriptor;
-    using edge_descriptor        = typename Graph::edge_descriptor;
-    using directed_category      = typename Graph::directed_category;
-    using edge_parallel_category = typename Graph::edge_parallel_category;
-    using traversal_category     = typename Graph::traversal_category;
-};
+template <typename Graph>
+struct IsBidirectional
+    :std::is_convertible<typename Graph::traversal_category, bidirectional_graph_tag> {};
 
 template <typename Graph>
-struct graph_traits<Graph, incidence_graph_tag> : graph_traits<Graph,void> {
+struct IsIncidence
+    :std::is_convertible<typename Graph::traversal_category, incidence_graph_tag> {};
+
+template <typename Graph>
+struct IsAdjacency
+    :std::is_convertible<typename Graph::traversal_category, adjacency_graph_tag> {};
+
+template <typename Graph>
+struct IsVertexList
+    :std::is_convertible<typename Graph::traversal_category, vertex_list_graph_tag> {};
+
+//default graph traits helper specializations depending on traversal category of the graph
+
+template <typename Graph, typename Base = void, typename Tag = void, typename EnableIf = void>
+struct graph_traits_helper:Base{};
+
+template <typename Graph>
+struct graph_traits_helper<Graph, void, void, std::enable_if_t<IsGraph<Graph>::value>> {
+    using vertex_descriptor = typename Graph::vertex_descriptor;
+    using edge_descriptor = typename Graph::edge_descriptor;
+    using directed_category = typename Graph::directed_category;
+    using edge_parallel_category = typename Graph::edge_parallel_category;
+    using traversal_category = typename Graph::traversal_category;
+    using vertices_size_type = typename Graph::vertices_size_type;
+    using edges_size_type = typename Graph::edges_size_type;
+};
+
+template <typename Graph, typename Base>
+struct graph_traits_helper<Graph, Base, bidirectional_tag, 
+    std::enable_if_t<IsBidirectional<Graph>::value>>:Base {
+    using in_edge_iterator = typename Graph::in_edge_iterator;
+};
+
+template <typename Graph, typename Base>
+struct graph_traits_helper<Graph, Base, incidence_graph_tag,
+    std::enable_if_t<IsIncidence<Graph>::value >> :Base {
     using out_edge_iterator = typename Graph::out_edge_iterator;
     using degree_size_type  = typename Graph::degree_size_type;
 };
 
-template <typename Graph>
-struct graph_traits<Graph, adjacency_graph_tag>: graph_traits<Graph,void> {
-    using adjacency_iterator = typename Graph::adjacency_iterator;    
+template <typename Graph, typename Base>
+struct graph_traits_helper<Graph, Base, adjacency_graph_tag,
+    std::enable_if_t<IsAdjacency<Graph>::value >> :Base {
+    using adjacency_iterator = typename Graph::adjacency_iterator;
 };
 
-template <typename Graph>
-struct graph_traits<Graph, vertex_list_graph_tag> : graph_traits<Graph, void> {
+template <typename Graph, typename Base>
+struct graph_traits_helper<Graph, Base, vertex_list_graph_tag,
+    std::enable_if_t<IsVertexList<Graph>::value >> :Base {
     using vertex_iterator    = typename Graph::vertex_iterator;
-    using vertices_size_type = typename Graph::vertices_size_type;
 };
 
+template <typename Graph, typename ... Tags>
+struct graph_traits_generator{};
+
+template <typename Graph, typename Tag, typename ... Tags> 
+struct graph_traits_generator<Graph, Tag, Tags...> {
+    using type = graph_traits_helper<Graph, 
+        typename graph_traits_generator<Graph, Tags...>::type,
+        Tag>;
+};
 template <typename Graph>
-struct graph_traits<Graph, bidirectional_graph_tag> : graph_traits<Graph, incidence_graph_tag> {
-    using in_edge_iterator = typename Graph::in_edge_iterator;    
+struct graph_traits_generator<Graph> {
+    using type = graph_traits_helper < Graph, void, void>;
+};
+
 };
 
 
+// default graph traits
+
+template <typename Graph> 
+struct graph_traits
+    : detail::graph_traits_generator<Graph, vertex_list_graph_tag, adjacency_graph_tag, 
+        incidence_graph_tag, bidirectional_graph_tag>::type {};
 };
