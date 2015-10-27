7505e6732ca9a712770a3a8837a949e159bb070c
Sergey Azovskov <last-g@yandex-team.ru>
2015-10-27 13:58:08 +0500

    Enable graph data

--- run_slaves.sh
+++ run_slaves.sh
@@ -3,15 +3,13 @@
 set -eu
 
 BASENAME='slave'
-NUM_OF_SLAVES=10
+NUM_OF_SLAVES=3
 
 for i in `seq 1 ${NUM_OF_SLAVES}`
 do
     NAME=${BASENAME}-${i}
-    CID=`sudo docker run -d --name "$NAME" -h "$NAME" lastg/rpclass-jenkins-slave`
+    CID=`sudo docker run -d --name "$NAME" -h "$NAME" -v /var/data:/var/data lastg/rpclass-jenkins-slave`
     docker inspect ${CID} | python -c "import sys,json; print(json.load(sys.stdin)[0]['NetworkSettings']['IPAddress'])"
 
 done
 
-
-#sudo docker run --rm -i -t -v `pwd`:/src lastg/graph-test
