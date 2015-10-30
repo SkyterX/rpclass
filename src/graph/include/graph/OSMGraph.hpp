f9563c5e325e53992f0ded1e31880a9690e8843e
Kuznetsov Artem <whoisnexta@gmail.com>
2015-10-30 13:33:49 +0500

    ddsg reader for OSMGraph

--- src/graph/include/graph/OSMGraph.hpp
+++ src/graph/include/graph/OSMGraph.hpp
@@ -4,6 +4,11 @@
 #include <cinttypes>
 #include <graph/io/IReader.hpp>
 #include <graph/properties.hpp>
+#include <fstream>
+#include <graph/io/FileReader.hpp>
+#include <graph/io/IReader.hpp>
+
+
 
 namespace graphOSM {
 	struct WeightTag {};
@@ -12,6 +17,10 @@ namespace graphOSM {
 	using OSMEdgeProperties = graph::Properties<graph::Property<WeightTag, OSMWeightType>>;
 	using OSMVertexProperties = graph::NoProperties;
 	using OSMGraph = graph::StaticGraph<OSMVertexProperties, OSMEdgeProperties>;
+
+	enum Direction {
+		DIR_BOTH = 0, DIR_TAIL_HEAD = 2, DIR_HEAD_TAIL = 1, DIR_CLOSED = 3
+	};
 
 	inline std::unique_ptr<OSMGraph> ReadGraphFrom(graphIO::IReader& reader) {
 
@@ -52,5 +61,38 @@ namespace graphOSM {
 		auto graph = builder->Build();
 
 		return graph;
+	}
+
+	inline std::unique_ptr<OSMGraph> ReadGraphFromDdsg(graphIO::IReader& reader) {
+		// read edge file header
+		std::string line = reader.ReadLine();
+		if (line != "d") {
+			std::cout << "expecting input file to start with a 'd'." << std::endl;
+			abort();
+		}
+
+		// read number of vertices and edges
+		graph::graph_traits<OSMGraph>::vertices_size_type vertexCount = reader.NextUnsignedInt();
+		graph::graph_traits<OSMGraph>::edges_size_type linksCount = reader.NextUnsignedInt();
+		std::cout << "ddsg graph has " << vertexCount << " nodes and " << linksCount << " edges." << std::endl;
+
+		std::unique_ptr<OSMGraph::Builder> builder = std::make_unique<OSMGraph::Builder>(vertexCount, linksCount);
+
+		while (reader.HasNext()) {
+			// each line = 4 ints = (from to weight direction)
+			uint32_t from = reader.NextUnsignedInt();
+			uint32_t to = reader.NextUnsignedInt();
+			uint32_t weight = reader.NextUnsignedInt();
+			uint32_t direction = reader.NextUnsignedInt();
+			OSMEdgeProperties edgeProperties;
+			graph::get<WeightTag>(edgeProperties) = weight;
+			if(direction == DIR_BOTH || direction == DIR_HEAD_TAIL) {
+				builder->AddEdge(from, to, edgeProperties);				
+			}
+			if(direction == DIR_BOTH || direction == DIR_TAIL_HEAD) {
+				builder->AddEdge(to, from, edgeProperties);
+			}
+		}
+		return builder->Build();
 	}
 }
