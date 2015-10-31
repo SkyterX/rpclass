a1c9c5493220b5624fac57b43c39673d397b1e04
Vitaly Osipov <developer@osipov.mobi>
2015-10-31 23:28:00 +0500

    Added forgotten color_map for the backward dijkstra

--- src/graph/include/graph/bidirectional_dijkstra.hpp
+++ src/graph/include/graph/bidirectional_dijkstra.hpp
@@ -5,18 +5,18 @@ namespace graph {
 
 
 template <typename PredecessorMapFTag, typename PredecessorMapBTag,
-    typename DisanceMapFTag, typename DisanceMapBTag, 
-    typename WeightMapTag, typename IndexMapTag, typename ColorMapTag,
+    typename DisanceMapFTag, typename DisanceMapBTag, typename WeightMapTag,
+    typename IndexMapTag, typename ColorMapFTag, typename ColorMapBTag,
     typename BundledVertexProperties, typename BundledEdgeProperties>
 struct GenerateBiDijkstraGraph {};
 
 template <typename PredecessorMapFTag, typename PredecessorMapBTag,
     typename DisanceMapFTag, typename DisanceMapBTag,
-    typename WeightMapTag, typename IndexMapTag, typename ColorMapTag,
-    typename... P1s, typename... P2s>
+    typename WeightMapTag, typename IndexMapTag, typename ColorMapFTag,
+    typename ColorMapBTag, typename... P1s, typename... P2s>
 struct GenerateBiDijkstraGraph<PredecessorMapFTag, PredecessorMapBTag,
     DisanceMapFTag, DisanceMapBTag, WeightMapTag,
-    IndexMapTag, ColorMapTag, Properties<P1s...>, Properties<P2s...>> {
+    IndexMapTag, ColorMapFTag, ColorMapBTag, Properties<P1s...>, Properties<P2s...>> {
     using type = StaticGraph<
         Properties<
             Property<PredecessorMapFTag, 
@@ -27,7 +27,8 @@ struct GenerateBiDijkstraGraph<PredecessorMapFTag, PredecessorMapBTag,
             Property<DisanceMapBTag, double>,
             Property<IndexMapTag, 
                 typename graph_traits<StaticGraph<Properties<>, Properties<>>>::vertices_size_type>,
-            Property<ColorMapTag, char>,
+            Property<ColorMapFTag, char>,
+            Property<ColorMapBTag, char>,
             P1s...>,
         Properties<
             Property<WeightMapTag, double>,
@@ -36,7 +37,7 @@ struct GenerateBiDijkstraGraph<PredecessorMapFTag, PredecessorMapBTag,
 
 template <class Graph, class PredecessorMapF, class PredecessorMapB,
 class DistanceMapF, class DistanceMapB, 
-class WeightMap, class IndexMap, class ColorMap, 
+class WeightMap, class IndexMap, class ColorMapF, class ColorMapB,
 class DijkstraVisitorF = DefaultDijkstraVisitor<Graph>, 
 class  DijkstraVisitorB = DefaultDijkstraVisitor<Graph>>
     void bidirectional_dijkstra(Graph& graph,
@@ -44,7 +45,7 @@ class  DijkstraVisitorB = DefaultDijkstraVisitor<Graph>>
         const typename graph_traits<Graph>::vertex_descriptor& t,
         PredecessorMapF& predecessorF, PredecessorMapB& predecessorB, 
         DistanceMapF& distanceF, DistanceMapB& distanceB, WeightMap& weight,
-        IndexMap& index, ColorMap& color, 
+        IndexMap& index, ColorMapF& colorF, ColorMapB& colorB,
         DijkstraVisitorF visitorF = DijkstraVisitorF(),
         DijkstraVisitorF visitorB = DijkstraVisitorB()) {};
 }
