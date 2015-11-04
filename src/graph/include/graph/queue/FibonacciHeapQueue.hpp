44bdb26eee55c024d4f3c88de40679698333d3d3
msamoylenko <ghost1212@mail.ru>
2015-11-05 00:54:44 +0500

    Fibonacci heap implementation. Upgraded PriorityQueue_Tests.

---
+++ src/graph/include/graph/queue/FibonacciHeapQueue.hpp
@@ -0,0 +1,221 @@
+#pragma once
+#include <graph/queue/PriorityQueue.hpp>
+#include <cassert>
+#include <vector>
+
+namespace graph {
+	namespace queue {
+		template <typename TKey, typename TDataId>
+		class FibonacciHeapQueue {
+		private:
+			using QueueItemType = QueueItem<TKey, TDataId>;
+
+			struct Node {
+				Node()
+					: Parent(nullptr), Child(nullptr),
+					  Left(nullptr), Right(nullptr),
+					  Degree(0), HadChildDeleted(false) { }
+
+				void Initialize(const TKey& key, const TDataId& data) {
+					this->Key = key;
+					this->DataId = data;
+					Left = this;
+					Right = this;
+				}
+
+				TKey Key;
+				TDataId DataId;
+				Node* Parent;
+				Node* Child;
+				Node* Left;
+				Node* Right;
+				int Degree;
+				bool HadChildDeleted;
+
+				bool IsNull() const {
+					return Left == nullptr;
+				}
+			};
+
+			struct Heap {
+				Node* Min;
+
+				Heap()
+					: Min(nullptr) {}
+
+				Heap(Node* node)
+					: Min(node) {
+					Min->Left = Min;
+					Min->Right = Min;
+				}
+
+				void Insert(Node* node) {
+					InsertRoot(node);
+				}
+
+				void DecreaseKey(Node* node, TKey newKey) {
+					node->Key = newKey;
+					if (node->Parent == nullptr) {
+						if (newKey < Min->Key)
+							Min = node;
+						return;
+					}
+					if (newKey >= node->Parent->Key)
+						return;
+					CascadeCut(node);
+				}
+
+				void CascadeCut(Node* node) {
+					Node* parent = node->Parent;
+					if (parent->Child == node)
+						parent->Child = (node == node->Right) ? nullptr : node->Right;
+					CutNode(node);
+					node->Parent = nullptr;
+					node->HadChildDeleted = false;
+					InsertRoot(node);
+
+					--parent->Degree;
+					if (!parent->HadChildDeleted || parent->Parent == nullptr) {
+						parent->HadChildDeleted = true;
+						return;
+					}
+					CascadeCut(parent);
+				}
+
+				void MergeNodes(Node* a, Node* b) {
+					LinkNodes(b->Left, a->Right);
+					LinkNodes(a, b);
+				}
+
+				void LinkNodes(Node* a, Node* b) {
+					a->Right = b;
+					b->Left = a;
+				}
+
+				void InsertRoot(Node* node) {
+					if (Min == nullptr) {
+						Min = node;
+					}
+					else {
+						MergeNodes(Min, node);
+						if (node->Key < Min->Key)
+							Min = node;
+					}
+				}
+
+				void CutNode(Node* node) {
+					LinkNodes(node->Left, node->Right);
+					node->Left = node;
+					node->Right = node;
+				}
+
+				void ExtractMin() {
+
+					if (Min->Child != nullptr) {
+						// Remove parent_ptr from childs
+						Node* child = Min->Child;
+						do {
+							child->Parent = nullptr;
+							child = child->Right;
+						}
+						while (child != Min->Child);
+
+						// Merge childs and and heap roots
+						MergeNodes(Min, child);
+						Min->Child = nullptr;
+					}
+
+					// Update heap size, roots list and min_ptr
+					Node* currentMin = Min;
+					Min = currentMin->Right != currentMin ? currentMin->Right : nullptr;
+					CutNode(currentMin);
+
+					// Fix structure
+					Consolidate();
+				}
+
+				std::vector<Node*> roots;
+
+				void Consolidate() {
+					if (Min == nullptr)
+						return;
+					// Decrease number of roots
+					roots.assign(50, nullptr);
+					Node* root = Min;
+					Node* nextRoot = root->Right;
+					do {
+						int d = root->Degree;
+						if (roots[d] == nullptr) {
+							roots[d] = root;
+							root = nextRoot;
+							nextRoot = root->Right;
+						}
+						else {
+							// choose root with smallest key
+							if (root->Key >= roots[d]->Key)
+								std::swap(root, roots[d]);
+							// fix min for cycle to end
+							if (roots[d] == Min)
+								Min = roots[d]->Right;
+							// add child
+							CutNode(roots[d]);
+							if (root->Child == nullptr)
+								root->Child = roots[d];
+							else
+								MergeNodes(root->Child, roots[d]);
+							++root->Degree;
+							roots[d] = nullptr;
+						}
+					}
+					while (roots[root->Degree] != root);
+
+					// Find min
+					Node* startRoot = Min;
+					root = Min->Right;
+					while (root != startRoot) {
+						if (root->Key < Min->Key)
+							Min = root;
+						root = root->Right;
+					}
+				}
+			};
+
+			Heap heap;
+			std::vector<Node> nodes;
+		public:
+
+			FibonacciHeapQueue(int dataIdSize) {
+				nodes.resize(dataIdSize);
+			}
+
+			void Insert(TKey key, TDataId dataId) {
+				assert(dataId < nodes.size());
+				assert(nodes[dataId].IsNull());
+				nodes[dataId].Initialize(key, dataId);
+				heap.Insert(&nodes[dataId]);
+			}
+
+			QueueItem<TKey, TDataId> PeekMin() const {
+				assert(!this->IsEmpty());
+				return MakeQueueItem(heap.Min->Key, heap.Min->DataId);
+			}
+
+			void DeleteMin() {
+				assert(!this->IsEmpty());
+				auto minDataId = heap.Min->DataId;
+				heap.ExtractMin();
+				nodes[minDataId] = Node();
+			}
+
+			void DecreaseKey(const TKey& key, const TDataId& dataId, const TKey& newKey) {
+				assert(dataId < nodes.size());
+				assert(!nodes[dataId].IsNull());
+				heap.DecreaseKey(&nodes[dataId], newKey);
+			}
+
+			bool IsEmpty() const {
+				return heap.Min == nullptr;
+			}
+		};
+	}
+}
--- src/graph/test/unit/PriorityQueue_Tests.cpp
+++ src/graph/test/unit/PriorityQueue_Tests.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include <graph/queue/SetQueue.hpp>
 #include <graph/queue/HeapQueue.hpp>
+#include <graph/queue/FibonacciHeapQueue.hpp>
 #include <graph/detail/util/Collection.hpp>
 #include <random>
 #include <type_traits>
@@ -11,37 +12,40 @@ using namespace std;
 using namespace graph::queue;
 using namespace graphUtil;
 
-const int TestItemsCount = 100000;
-const int TestDecreaseCount = 10 * TestItemsCount;
+const int TestItemsCount = 10000;
+const int DecreaseQueriesCount = 10 * TestItemsCount;
+const int RandomQueriesCount = 100 * TestItemsCount;
 const int RandomSeed = 231274120;
 const int MaxKey = 1e9;
 const int MinStartKey = 1e6;
 const int MinDecreasedKey = 0;
+const double DeleteMinProbability = 0.2;
 
-uniform_int_distribution<size_t> ItemIdDistribution(0, TestItemsCount-1);
+uniform_int_distribution<size_t> ItemIdDistribution(0, TestItemsCount - 1);
+uniform_real_distribution<double> Probability(0.0, 1.0);
 
 using KeyType = uint32_t;
 using DataType = uint32_t;
 using KeyDistribution = uniform_int_distribution<KeyType>;
 
-template<typename TQueue>
-void TestQueue(TQueue& queue) {
+template <typename TQueue>
+void Queue_SequentialTest(TQueue& queue) {
 	mt19937 generator(RandomSeed);
 	KeyDistribution startKeyDistribution(MinStartKey, MaxKey);
 
 	vector<pair<KeyType, DataType>> items;
 	for (auto x : Range(0, TestItemsCount)) {
 		KeyType key = startKeyDistribution(generator);
-		items.push_back(make_pair(key,x));
+		items.push_back(make_pair(key, x));
 		queue.Insert(key, x);
 	}
 
-	for(auto iterationId : Range(0, TestDecreaseCount)) {
+	for (auto iterationId : Range(0, DecreaseQueriesCount)) {
 		auto itemId = ItemIdDistribution(generator);
 		auto item = items[itemId];
-		if (item.first == 0) 
+		if (item.first == 0)
 			continue;
-		KeyDistribution newKeyDistribution(MinDecreasedKey, item.first-1);
+		KeyDistribution newKeyDistribution(MinDecreasedKey, item.first - 1);
 		KeyType newKey = newKeyDistribution(generator);
 		queue.DecreaseKey(item.first, item.second, newKey);
 		items[itemId].first = newKey;
@@ -57,12 +61,71 @@ void TestQueue(TQueue& queue) {
 	EXPECT_TRUE(queue.IsEmpty());
 }
 
-TEST(PriorityQueue, Set) {
-	auto queue = SetQueue<KeyType, DataType>();
-	TestQueue(queue);
+template <typename TQueue>
+void Queue_RandomTest(TQueue& queue) {
+	mt19937 generator(RandomSeed);
+	KeyDistribution startKeyDistribution(MinStartKey, MaxKey);
+	vector<pair<KeyType, DataType>> items(TestItemsCount);
+	vector<bool> isEnqueued(TestItemsCount);
+
+	for (auto x : Range(0, TestItemsCount)) {
+		KeyType key = startKeyDistribution(generator);
+		items.push_back(make_pair(key, x));
+		isEnqueued.push_back(false);
+	}
+
+	for (auto iterationId : Range(0, RandomQueriesCount)) {
+		if (!queue.IsEmpty() && Probability(generator) < DeleteMinProbability) {
+			isEnqueued[queue.PeekMin().Data()] = false;
+			queue.DeleteMin();
+			continue;
+		}
+		auto itemId = ItemIdDistribution(generator);
+		auto& item = items[itemId];
+		if (!isEnqueued[item.second]) {
+			item.first = startKeyDistribution(generator);
+			queue.Insert(item.first, item.second);
+			isEnqueued[item.second] = true;
+		}
+		else {
+			if (item.first == 0)
+				continue;
+			KeyDistribution newKeyDistribution(MinDecreasedKey, item.first - 1);
+			KeyType newKey = newKeyDistribution(generator);
+			queue.DecreaseKey(item.first, item.second, newKey);
+			item.first = newKey;
+		}
+	}
+
+	sort(items.begin(), items.end());
 }
 
-TEST(PriorityQueue, Heap) {
+TEST(PriorityQueue, Set_Sequential) {
+	auto queue = SetQueue<KeyType, DataType>();
+	Queue_SequentialTest(queue);
+}
+
+TEST(PriorityQueue, Set_Random) {
+	auto queue = SetQueue<KeyType, DataType>();
+	Queue_RandomTest(queue);
+}
+
+TEST(PriorityQueue, Heap_Sequential) {
 	auto queue = HeapQueue<KeyType, DataType>(TestItemsCount);
-	TestQueue(queue);
+	Queue_SequentialTest(queue);
+}
+
+TEST(PriorityQueue, Heap_Random) {
+	auto queue = HeapQueue<KeyType, DataType>(TestItemsCount);
+	Queue_RandomTest(queue);
+}
+
+TEST(PriorityQueue, Fibonacci_Sequential) {
+	auto queue = FibonacciHeapQueue<KeyType, DataType>(TestItemsCount);
+	Queue_SequentialTest(queue);
+}
+
+TEST(PriorityQueue, Fibonacci_Random) {
+	auto queue = FibonacciHeapQueue<KeyType, DataType>(TestItemsCount);
+	Queue_RandomTest(queue);
 }
