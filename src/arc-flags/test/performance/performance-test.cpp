82653dd56b65fca60a193d1b97be4bc98d797e2b
Vitaly Osipov <developer@osipov.mobi>
2015-11-05 15:22:08 +0500

    Real arc flags tests

--- src/arc-flags/test/performance/performance-test.cpp
+++ src/arc-flags/test/performance/performance-test.cpp
@@ -1,6 +1,120 @@
+#include <type_traits>
+#include <utility>
+#include <cassert>
+#include <queue>
+#include <vector>
+#include <string>
+#include <type_traits>
 #include <gtest/gtest.h>
+#include <graph/static_graph.hpp>
+#include <graph/graph.hpp>
+#include <graph/properties.hpp>
+#include <graph/io.hpp>
+#include <arc-flags/arc-flags.hpp>
+
+using namespace std;
+using namespace graph;
+using namespace arcflags;
+
+struct distance_t {};
+struct color_t {};
+struct predecessor_t {};
+struct weight_t {};
+struct partition_t {};
+struct arc_flags_t {};
+
+
+std::string baseFileName(const std::string& path) {
+    std::string str = path.substr(path.find_last_of("/\\") + 1);
+    std::string::size_type const p(str.find_last_of('.'));
+    std::string base = str.substr(0, p);
+    return base;
+}
+
+char* globalPathToFiles = nullptr;
+
+class DdsgGraphAlgorithm : public ::testing::TestWithParam<tuple<const char*,double>> {
+protected:
+    DdsgGraphAlgorithm()
+        :m_ddsgVecBackInserter(m_ddsgVec), m_path(globalPathToFiles),
+        m_fileName(get<0>(GetParam())),
+        m_baseName(baseFileName(m_fileName)),
+        m_filter(get<1>(GetParam())){};
+    virtual void SetUp() {
+        if (read_ddsg<Property<weight_t, uint32_t>>(m_ddsgVecBackInserter,
+            m_numOfNodes, m_numOfEdges, (m_path + "/" + m_fileName).c_str()))
+            FAIL();
+        std::sort(m_ddsgVec.begin(), m_ddsgVec.end(),
+            [&](DdsgVecType::value_type left, DdsgVecType::value_type right) {
+            return left.first.first < right.first.first;
+        });
+    };
+
+    using DdsgVecType = std::vector<std::pair<std::pair<size_t, size_t>, Property<weight_t, uint32_t>>>;
+    using N = integral_constant<size_t, 8>;
+    DdsgVecType m_ddsgVec;
+    back_insert_iterator<DdsgVecType> m_ddsgVecBackInserter;
+    string m_path;
+    string m_fileName;
+    string m_baseName;
+    double m_filter;
+    size_t m_numOfNodes;
+    size_t m_numOfEdges;
+};
+
+TEST_P(DdsgGraphAlgorithm, ArcFlags) {
+    using Graph = GenerateArcFlagsGraph<predecessor_t, distance_t, weight_t,
+        vertex_index_t, color_t, arc_flags_t, partition_t, N::value,
+        Properties<>, Properties< >> ::type;
+    Graph graph(m_ddsgVec.begin(), m_ddsgVec.end(), m_numOfNodes, m_numOfEdges);    
+    auto predecessor = graph::get(predecessor_t(), graph);
+    auto distance = graph::get(distance_t(), graph);
+    auto weight = graph::get(weight_t(), graph);
+    auto vertex_index = graph::get(vertex_index_t(), graph);
+    auto color = graph::get(color_t(), graph);
+    auto partition = graph::get(partition_t(), graph);
+    auto arc_flags = graph::get(arc_flags_t(), graph);
+    stringstream ss;    
+    ss << m_path << "/" << m_baseName << "/tmppartition" << N::value;    
+    if (read_partitioning<N::value, partition_t>(graph, ss.str().c_str())) {
+        FAIL();
+    };
+    arcflags_preprocess<N::value>(graph, predecessor, distance, weight, vertex_index, 
+        color, partition, arc_flags, m_filter);    
+
+    ifstream verificationFile;    
+    ss.str(string());
+    ss << m_path << "/" << m_baseName << "/" << m_baseName << ".ppsp";
+    verificationFile.open(ss.str());
+
+    if (!verificationFile.is_open()) {
+        cerr << "Verification file " << ss.str() << " is not found." << endl;
+        FAIL();
+    };
+    size_t src, tgt, dis;
+    while (verificationFile >> src >> tgt >> dis) {
+        cout << "Running ArcFlags query from " << src << " to " << tgt << endl;
+        arcflags_query<N::value>(graph,
+            graph_traits<Graph>::vertex_descriptor(src),
+            graph_traits<Graph>::vertex_descriptor(tgt),
+            predecessor, distance, weight, vertex_index,
+            color, partition, arc_flags);
+        EXPECT_EQ(dis, get(distance, tgt));
+    }
+    verificationFile.close();
+};
+
+
+INSTANTIATE_TEST_CASE_P(CommandLine, DdsgGraphAlgorithm,
+    ::testing::Combine(::testing::Values("bel.ddsg"), ::testing::Values(0.0)));
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
+    if (argc >= 2) globalPathToFiles = argv[1];
+    else {
+        cerr << "Path to the folder with .ddsg graphs is required" << endl;
+        return 1;
+    };
+
     return RUN_ALL_TESTS();
 };
--- src/arc-flags/test/unit/main-test.cpp
+++ src/arc-flags/test/unit/main-test.cpp
@@ -1,35 +1,7 @@
 #include <gtest/gtest.h>
 #include <graph/static_graph.hpp>
-//#include <graph/io.hpp>
 #include <arc-flags/arc-flags.hpp>
 
-struct distance_t {};
-struct color_t {};
-struct edge_type_t {};
-struct predecessor_t {};
-struct weight_t {};
-struct arc_flags_t {};
-struct partition_t {};
-using namespace arcflags;
-
-TEST(GraphAlgorithms, ArcFlags) {
-    constexpr size_t NumOfParts = 64;
-    using namespace graph;
-    using Graph = GenerateArcFlagsGraph<predecessor_t, distance_t, weight_t,
-        vertex_index_t, color_t, arc_flags_t,partition_t, NumOfParts,
-        Properties<>, Properties<>> ::type;
-    Graph graph;
-    auto predecessor = graph::get(predecessor_t(), graph);
-    auto distance = graph::get(distance_t(), graph);
-    auto weight = graph::get(weight_t(), graph);
-    auto vertex_index = graph::get(vertex_index_t(), graph);
-    auto color = graph::get(color_t(), graph);
-    auto partition = graph::get(partition_t(), graph);
-    auto arcflags = graph::get(arc_flags_t(), graph);
-//    read_ddsg<weight_t>(graph, "PathToFile");
-    read_partitioning<NumOfParts, partition_t>(graph, "PathToFile");
-    arcflags_preprocess<NumOfParts>(graph, predecessor, distance, weight, vertex_index, color, partition, arcflags);
-};
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
