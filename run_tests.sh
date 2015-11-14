04b59689b7b7136d4e623f3d485ba8e423768d08
Sergey Azovskov <last-g@yandex-team.ru>
2015-11-14 20:21:11 +0500

    RUn tests from chosen dir

--- run_tests.sh
+++ run_tests.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 GRAPHS_FOLDER="/var/data/"
+BASE_DIR=${1:-build}
+echo  $BASE_DIR
 
-for tst in build/bin/*-{unit,other}
+for tst in ${BASE_DIR}/bin/*-{unit,other}
 do
     if [[ -e "$tst" ]];
     then
