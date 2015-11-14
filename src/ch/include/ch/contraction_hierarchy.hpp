8c512b30fe93be7c38684ae3c45e5a423f777b46
Vitaly Osipov <developer@osipov.mobi>
2015-11-14 19:36:32 +0500

    Contraction hierarchy sample test and generator

--- src/ch/include/ch/contraction_hierarchy.hpp
+++ src/ch/include/ch/contraction_hierarchy.hpp
@@ -1,1 +1,79 @@
 #pragma once
+
+#include <graph/dijkstra.hpp>
+#include <graph/dynamic_graph.hpp>
+#include <boost/graph/two_bit_color_map.hpp>
+
+namespace ch {
+
+template <typename PredecessorMapTag, class DisanceMapTag, typename WeightMapTag,
+    typename IndexMapTag, typename ColorMapTag, typename UnPackMapTag,
+    typename VertexOrderMapTag, typename DirectionMapTag,
+    typename VP, typename EP, typename GP>
+struct GenerateCHGraph {};
+
+template <typename PredecessorMapTag, class DisanceMapTag, typename WeightMapTag,
+    typename IndexMapTag, typename ColorMapTag, typename UnPackMapTag,
+    typename VertexOrderMapTag, typename DirectionMapTag,
+    typename... P1s, typename... P2s, typename ... P3s>
+struct GenerateCHGraph<PredecessorMapTag, DisanceMapTag, WeightMapTag,
+    IndexMapTag, ColorMapTag, UnPackMapTag, VertexOrderMapTag, DirectionMapTag,
+    graph::Properties<P1s...>, graph::Properties<P2s...>, graph::Properties<P3s... >> {
+    using EmptyGraph = graph::DynamicGraph <
+        graph::Properties<>, graph::Properties<>, graph::Properties<> >;
+
+    using vertex_descriptor =
+        typename graph::graph_traits<EmptyGraph>::vertex_descriptor;
+    using vertices_size_type =
+        typename graph::graph_traits<EmptyGraph>::vertices_size_type;
+
+    using type = graph::DynamicGraph <
+        graph::Properties<
+            graph::Property<PredecessorMapTag, vertex_descriptor>,
+            graph::Property<DisanceMapTag, uint32_t>,
+            graph::Property<ColorMapTag, boost::two_bit_color_type>,
+            graph::Property<VertexOrderMapTag, vertices_size_type>,
+            graph::Property<DirectionMapTag, char>,
+            P1s...>,
+        graph::Properties<
+            graph::Property<WeightMapTag, uint32_t>,
+            graph::Property<UnPackMapTag, vertex_descriptor>,
+            P2s...>,
+        graph::Properties<P3s...>
+    >;
+};
+
+template <typename Graph>
+class DefaultOrderStrategy {
+public:
+    //DefaultOrderStrategy(DefaultOrderStrategy&& other){
+    //    *this(other); 
+    //};
+    typename graph::graph_traits<Graph>::vertex_descriptor
+        next(Graph& graph) {};
+
+};
+
+// uses dijkstra, therefore should have at least all property maps used by dijkstra
+template <typename Graph, typename PredecessorMap, typename DistanceMap,
+    typename WeightMap, typename IndexMap, typename ColorMap, typename UnPackMap,
+    typename VertexOrderMap, typename DirectionMap,
+    typename OrderStrategy = DefaultOrderStrategy<Graph>>
+    void ch_preprocess(Graph& graph, PredecessorMap& predecessor, DistanceMap& distance,
+        WeightMap& weight, IndexMap& index, ColorMap& color, UnPackMap& unpack,
+        VertexOrderMap& order, DirectionMap& direction,
+        OrderStrategy&& strategy = OrderStrategy()) {};
+
+
+template <typename Graph, typename PredecessorMap, typename DistanceMap,
+    typename WeightMap, typename IndexMap, typename ColorMap, typename UnPackMap,
+    typename VertexOrderMap, typename DirectionMap>
+    void ch_query(Graph& graph, 
+        const typename graph::graph_traits<Graph>::vertex_descriptor& s,
+        const typename graph::graph_traits<Graph>::vertex_descriptor& t,
+        PredecessorMap& predecessor, DistanceMap& distance,
+        WeightMap& weight, IndexMap& index, ColorMap& color, UnPackMap& unpack,
+        VertexOrderMap& order, DirectionMap& direction) {
+        graph::dijkstra(graph, s, predecessor, distance, weight, index, color);
+    };
+};
--- src/ch/test/performance/performance-test.cpp
+++ src/ch/test/performance/performance-test.cpp
@@ -1,10 +1,118 @@
-#include <gtest/gtest.h>
-#include <graph/static_graph.hpp>
-#include <ch/contraction_hierarchy.hpp>
+#include <type_traits>
+#include <utility>
+#include <cassert>
+#include <queue>
+#include <vector>
+#include <string>
 
 #include <gtest/gtest.h>
+#include <graph/io.hpp>
+#include <ch/contraction_hierarchy.hpp>
+
+
+#include <gtest/gtest.h>
+
+using namespace std;
+using namespace graph;
+using namespace ch;
+
+struct distance_t {};
+struct color_t {};
+struct predecessor_t {};
+struct weight_t {};
+struct unpack_t {};
+struct vertex_order_t {};
+struct direction_t {};
+
+std::string baseFileName(const std::string& path) {
+    std::string str = path.substr(path.find_last_of("/\\") + 1);
+    std::string::size_type const p(str.find_last_of('.'));
+    std::string base = str.substr(0, p);
+    return base;
+}
+
+char* globalPathToFiles = nullptr;
+
+class DdsgGraphAlgorithm : public ::testing::TestWithParam<tuple<const char*, double>> {
+protected:
+    DdsgGraphAlgorithm()
+        :m_ddsgVecBackInserter(m_ddsgVec), m_path(globalPathToFiles),
+        m_fileName(get<0>(GetParam())),
+        m_baseName(baseFileName(m_fileName)),
+        m_filter(get<1>(GetParam())) {};
+    virtual void SetUp() {
+        if (read_ddsg<Property<weight_t, uint32_t>>(m_ddsgVecBackInserter,
+            m_numOfNodes, m_numOfEdges, (m_path + "/" + m_fileName).c_str()))
+            FAIL();
+        std::sort(m_ddsgVec.begin(), m_ddsgVec.end(),
+            [&](DdsgVecType::value_type left, DdsgVecType::value_type right) {
+            return left.first.first < right.first.first;
+        });
+    };
+
+    using DdsgVecType = std::vector<std::pair<std::pair<size_t, size_t>, Property<weight_t, uint32_t>>>;
+    using N = integral_constant<size_t, 8>;
+    DdsgVecType m_ddsgVec;
+    back_insert_iterator<DdsgVecType> m_ddsgVecBackInserter;
+    string m_path;
+    string m_fileName;
+    string m_baseName;
+    double m_filter;
+    size_t m_numOfNodes;
+    size_t m_numOfEdges;
+};
+
+TEST_P(DdsgGraphAlgorithm, CH) {
+    using Graph = GenerateCHGraph<predecessor_t, distance_t, weight_t,
+        vertex_index_t, color_t, unpack_t, vertex_order_t, direction_t,
+        Properties<>, Properties<>, Properties<>> ::type;
+    Graph graph(m_ddsgVec.begin(), m_ddsgVec.end(), m_numOfNodes, m_numOfEdges);
+    auto predecessor = graph::get(predecessor_t(), graph);
+    auto distance = graph::get(distance_t(), graph);
+    auto weight = graph::get(weight_t(), graph);
+    auto vertex_index = graph::get(vertex_index_t(), graph);
+    auto color = graph::get(color_t(), graph);
+    auto unpack = graph::get(unpack_t(), graph);
+    auto order = graph::get(vertex_order_t(), graph);
+    auto direction = graph::get(direction_t(), graph);
+    stringstream ss;
+    
+    ch_preprocess<Graph>(graph, predecessor, distance, weight, vertex_index,
+        color, unpack, order, direction);
+
+    ifstream verificationFile;
+    ss.str(string());
+    ss << m_path << "/" << m_baseName << "/" << m_baseName << ".ppsp";
+    verificationFile.open(ss.str());
+
+    if (!verificationFile.is_open()) {
+        cerr << "Verification file " << ss.str() << " is not found." << endl;
+        FAIL();
+    };
+    size_t src, tgt, dis;
+    while (verificationFile >> src >> tgt >> dis) {
+        cout << "Running CH query from " << src << " to " << tgt << endl;
+        ch_query(graph,
+            graph_traits<Graph>::vertex_descriptor(src),
+            graph_traits<Graph>::vertex_descriptor(tgt),
+            predecessor, distance, weight, vertex_index,
+            color, unpack, order, direction);
+        EXPECT_EQ(dis, get(distance, tgt));
+    }
+    verificationFile.close();
+};
+
+
+INSTANTIATE_TEST_CASE_P(CommandLine, DdsgGraphAlgorithm,
+    ::testing::Combine(::testing::Values("deu.ddsg"), ::testing::Values(0.0)));
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
+    if (argc >= 2) globalPathToFiles = argv[1];
+    else {
+        cerr << "Path to the folder with .ddsg graphs is required" << endl;
+        return 1;
+    };
+
     return RUN_ALL_TESTS();
-}
+};
