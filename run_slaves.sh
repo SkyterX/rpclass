49f19e8a4c92e615986a06bae64af38a8fed50b5
Sergey Azovskov <last-g@yandex-team.ru>
2015-11-14 20:59:31 +0500

    Update runslaves to actual

--- run_slaves.sh
+++ run_slaves.sh
@@ -8,7 +8,7 @@ NUM_OF_SLAVES=3
 for i in `seq 1 ${NUM_OF_SLAVES}`
 do
     NAME=${BASENAME}-${i}
-    CID=`sudo docker run -d --name "$NAME" -h "$NAME" -v /var/data:/var/data lastg/rpclass-jenkins-slave`
+    CID=`sudo docker run -d --name "$NAME" -h "$NAME" -v /var/data:/var/data -p "172.17.42.1:5130${i}:22" lastg/rpclass-jenkins-slave`
     docker inspect ${CID} | python -c "import sys,json; print(json.load(sys.stdin)[0]['NetworkSettings']['IPAddress'])"
 
 done
