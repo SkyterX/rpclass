7fd9797a9f9e52a4afd1863afdf11fbe781e9416
Sergey Azovskov <last-g@yandex-team.ru>
2015-11-14 21:52:47 +0500

    Runner for perf tests

---
+++ run_perf_tests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+GRAPHS_FOLDER="/var/data/"
+BASE_DIR=${1:-build}
+echo  $BASE_DIR
+
+for tst in ${BASE_DIR}/bin/*-performance
+do
+    if [[ -e "$tst" ]];
+    then
+        echo Running $tst
+        "$tst" --gtest_output=xml:${tst}_details.xml $GRAPHS_FOLDER
+    fi
+done
