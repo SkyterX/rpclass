21db6965eac7d18f091ce0949d769d09b1cd7204
Vitaly Osipov <developer@osipov.mobi>
2015-11-15 22:36:58 +0500

    Generate statistics in ltsv format and write to statistics file

--- src/util/include/util/statistics.h
+++ src/util/include/util/statistics.h
@@ -5,29 +5,65 @@
 namespace util{
 namespace statistics {
 
+enum class GeneralKeys : char {
+    graph,
+    algorithm,
+    phase, 
+    metric, 
+    vertices,
+    edges,
+    time,
+    space
+};
+
+std::ostream& operator<<(std::ostream& osm, const GeneralKeys& arg) {
+    switch (arg) {
+    case GeneralKeys::algorithm:
+        osm << "algorithm";
+        break;
+    case GeneralKeys::edges:
+        osm << "edges";
+        break;
+    case GeneralKeys::graph:
+        osm << "graph";
+        break;
+    case GeneralKeys::metric:
+        osm << "metric";
+        break;
+    case GeneralKeys::phase:
+        osm << "phase";
+        break;
+    case GeneralKeys::space:
+        osm << "space";
+        break;
+    case GeneralKeys::time:
+        osm << "time";
+        break;
+    case GeneralKeys::vertices:
+        osm << "vertices";
+        break;
+    default:
+        osm << "Unknown column";
+        break;
+    };
+    return osm;
+};
+
+
 enum class Algorithm: char {
-    bfs = 0,
-    dijkstra = 1,
-    dijkstraPtoP = 2,
-    biDijkstra = 3
+    bfs,
+    dijkstra,
+    dijkstraPtoP,
+    biDijkstra
 };
 
-enum class Metric : char {
-    time = 0
-};
-
-enum class Phase : char {
-    topology = 0,
-    metric = 1,
-    query = 2
-};
 
 std::ostream& operator<<(std::ostream& osm, const Algorithm& arg) {
     switch (arg) {
     case Algorithm::bfs:
         osm << "bfs";
         break;
-    case Algorithm::dijkstra: 
+    case Algorithm::dijkstra:
         osm << "dijkstra";
         break;
     case Algorithm::dijkstraPtoP:
@@ -40,20 +76,12 @@ std::ostream& operator<<(std::ostream& osm, const Algorithm& arg) {
         osm << "Unknown algorithm";
         break;
     };
-return osm;
+    return osm;
 };
-
-
-std::ostream& operator<<(std::ostream& osm, const Metric& arg) {
-    switch (arg) {
-    case Metric::time:
-        osm << "time";
-        break;
-    default:
-        osm << "unkonwn metric";
-        break;
-    };
-return osm;
+enum class Phase : char {
+    topology,
+    metric,
+    query
 };
 
 std::ostream& operator<<(std::ostream& osm, const Phase& arg) {
@@ -74,30 +102,64 @@ std::ostream& operator<<(std::ostream& osm, const Phase& arg) {
     return osm;;
 };
 
+enum class Metric : char {
+    time
+};
+
+std::ostream& operator<<(std::ostream& osm, const Metric& arg) {
+    switch (arg) {
+    case Metric::time:
+        osm << "time";
+        break;
+    default:
+        osm << "unknown metric";
+        break;
+    };
+return osm;
+};
+
+
+template <typename Key, typename Value>
+struct StatisticsField {
+    StatisticsField(Key key, const Value& value)
+    :key(key),value(value){};
+    Key key;
+    Value value;
+};
+
+template <typename Key, typename Value>
+std::ostream& operator<<(std::ostream& osm, const StatisticsField<Key, Value>& arg) {
+    osm << arg.key << ":" << arg.value;
+    return osm;
+};
+
+
 struct GeneralStatistics {
-    std::string graphName;
-    Algorithm algorithm;
-    Phase phase;
-    Metric metric;
-    uint64_t n, m, time, space;
-    bool turns = false;
-    GeneralStatistics(const std::string& graphName,
+    StatisticsField<GeneralKeys,std::string> graph;
+    StatisticsField<GeneralKeys,Algorithm> algorithm;
+    StatisticsField<GeneralKeys,Phase> phase;
+    StatisticsField<GeneralKeys,Metric> metric;
+    StatisticsField<GeneralKeys,uint64_t> vertices;
+    StatisticsField<GeneralKeys,uint64_t> edges;
+    StatisticsField<GeneralKeys,uint64_t> time;
+    StatisticsField<GeneralKeys,uint64_t> space;
+    GeneralStatistics(const std::string& graph,
         const Algorithm& algorithm, const Phase& phase, const Metric& metric,
-        const uint64_t& n, uint64_t m, const uint64_t& time,
-        const uint64_t& space, bool turns)
-        :graphName(graphName),
-        algorithm(algorithm),
-        phase(phase),
-        metric(metric),
-        n(n), m(m),
-        time(time),
-        space(space),
-        turns(turns) {};
+        const uint64_t& vertices, uint64_t edges, const uint64_t& time,
+        const uint64_t& space)
+        :graph(GeneralKeys::graph,graph),
+        algorithm(GeneralKeys::algorithm,algorithm),
+        phase(GeneralKeys::phase,phase),
+        metric(GeneralKeys::metric, metric),
+        vertices(GeneralKeys::vertices,vertices), 
+        edges(GeneralKeys::edges,edges),
+        time(GeneralKeys::time,time),
+        space(GeneralKeys::space,space) {};
 };
 
 std::ostream& operator<<(std::ostream& osm, const GeneralStatistics& arg) {
-    osm << arg.graphName << ',' << arg.algorithm << ',' << arg.phase << ',' << arg.metric << ',' <<
-        arg.n << ','<< arg.m << ','<< arg.time << ',' << arg.space << ',' << arg.turns;
+    osm << arg.graph << '\t' << arg.algorithm << '\t' << arg.phase << '\t' << arg.metric << '\t' <<
+        arg.vertices << '\t'<< arg.edges << '\t'<< arg.time << '\t' << arg.space;
 return osm;
 };
 
