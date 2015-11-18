4104a9367067c4dcde67b9d580ecd2f731e04032
msamoylenko <ghost1212@mail.ru>
2015-11-18 21:35:45 +0500

    Improved arc-flags reduction.

--- src/arc-flags/include/arc-flags/Bitset.hpp
+++ src/arc-flags/include/arc-flags/Bitset.hpp
@@ -2,29 +2,47 @@
 
 #include <bitset>
 
-namespace arcflags
-{
-	namespace bitset
-	{
+namespace arcflags {
+	namespace bitset {
 		template <size_t size>
-		class Bitset
-		{
+		class Bitset {
+			using BitsetType = std::bitset<size>;
 		public:
-			Bitset(){}
+			Bitset() {}
 
-			bool GetBit(size_t position) const
-			{
+			bool GetBit(size_t position) const {
 				return bitset[position];
 			}
 
-			void SetBit(size_t position, bool value = true)
-			{
+			void SetBit(size_t position, bool value = true) {
 				bitset.set(position, value);
 			}
 
+			size_t GetHashCode() const {
+				return hasher(bitset);
+			}
+
+			friend bool operator==(const Bitset& lhs, const Bitset& rhs) {
+				return lhs.bitset == rhs.bitset;
+			}
+
+			friend bool operator!=(const Bitset& lhs, const Bitset& rhs) {
+				return !(lhs == rhs);
+			}
+
 		private:
-			std::bitset<size> bitset;
+			const static std::hash<BitsetType> hasher;
+			BitsetType bitset;
 		};
 	}
 }
 
+namespace std {
+	template <size_t size>
+	struct hash<arcflags::bitset::Bitset<size>>
+		: public unary_function<arcflags::bitset::Bitset<size>, size_t> {
+		size_t operator()(const arcflags::bitset::Bitset<size>& bitset) const {
+			return bitset.GetHashCode();
+		}
+	};
+}
--- src/arc-flags/include/arc-flags/arc-flagsReduction.hpp
+++ src/arc-flags/include/arc-flags/arc-flagsReduction.hpp
@@ -1,31 +1,28 @@
 #pragma once
-#include <cstdio>
 #include <vector>
 #include <queue>
-#include <map>
+#include <unordered_map>
+#include <unordered_set>
 #include <algorithm>
 #include <cmath>
 #include <graph/static_graph.hpp>
-#include <graph/dijkstra.hpp>
-#include <arc-flags/Bitset.hpp>
-#include <graph/io/FileReader.hpp>
-#include <graph/graph.hpp>
 #include <graph/properties.hpp>
 #include <graph/detail/ComplementGraph.hpp>
 
 namespace arcflags
 {
+	template <size_t N, typename ArcFlag>
+	size_t CountBits(const ArcFlag& x) {
+		size_t bits = 0;
+		for (size_t bitIndex = 0; bitIndex < N; ++bitIndex) {
+			if (x.GetBit(bitIndex))
+				++bits;
+		}
+		return bits;
+	}
+
 	template <typename ArcFlag, size_t N>
 	struct ArcFlagComparer {
-		size_t CountBits(const ArcFlag& x) const {
-			size_t bits = 0;
-			for (size_t bitIndex = 0; bitIndex < N; ++bitIndex) {
-				if (x.GetBit(bitIndex))
-					++bits;
-			}
-			return bits;
-		}
-
 		bool LexicographicalCompare(const ArcFlag& x, const ArcFlag& y) const {
 			for (size_t bitIndex = 0; bitIndex < N; ++bitIndex) {
 				if (x.GetBit(bitIndex) != y.GetBit(bitIndex))
@@ -35,9 +32,41 @@ namespace arcflags
 		}
 
 		bool operator()(const ArcFlag& x, const ArcFlag& y) const {
-			auto xBits = CountBits(x);
-			auto yBits = CountBits(y);
+			auto xBits = CountBits<N>(x);
+			auto yBits = CountBits<N>(y);
 			return xBits < yBits || (xBits == yBits && LexicographicalCompare(x, y));
+		}
+	};
+
+	template <typename ArcFlag, size_t N>
+	struct ArcFlagStorage {
+		std::unordered_set<ArcFlag> ArcFlags;
+		std::vector<ArcFlag> ArcFlagsByBits[N + 1];
+
+		void Add(const ArcFlag& arcFlag) {
+			if (ArcFlags.insert(arcFlag).second)
+				ArcFlagsByBits[CountBits<N>(arcFlag)].push_back(arcFlag);
+		}
+
+		const ArcFlag& FindClosest(const ArcFlag& arcFlag) {
+			if (ArcFlags.find(arcFlag) != ArcFlags.end())
+				return arcFlag;
+			for (int bits = CountBits<N>(arcFlag); bits < N; ++bits) {
+				for (const auto& storedArcFlag : ArcFlagsByBits[bits]) {
+					bool isValid = true;
+					for (size_t bitIndex = 0; bitIndex < N; ++bitIndex) {
+						if (arcFlag.GetBit(bitIndex) && !storedArcFlag.GetBit(bitIndex)) {
+							isValid = false;
+							break;
+						}
+					}
+					if (isValid) {
+						return storedArcFlag;
+					}
+				}
+			}
+			assert(false);
+			return arcFlag;
 		}
 	};
 
@@ -51,15 +80,20 @@ namespace arcflags
 	}
 
 	template <size_t N, typename ArcFlag, typename ArcFlagMap>
-	void FindClosestArcFlag(const ArcFlag& startArcFlag, ArcFlagMap& arcFlagMap) {
+	void FindClosestArcFlag(const ArcFlag& startArcFlag, ArcFlagMap& arcFlagMap, ArcFlagStorage<ArcFlag, N>& selectedArcFlags) {
 		using namespace std;
 		using namespace graph;
 		using namespace graphUtil;
+		const int maxDistance = 1;
 
-		std::queue<ArcFlag> q;
-		q.push(startArcFlag);
-		while (!q.empty()) {
-			auto arcFlag = q.front();
+		std::queue<pair<ArcFlag, int>> q;
+		q.push(make_pair(startArcFlag, 0));
+		int iterationId = 0;
+		while (!q.empty() && iterationId < selectedArcFlags.ArcFlags.size()) {
+			++iterationId;
+			ArcFlag arcFlag;
+			int distance;
+			std::tie(arcFlag, distance) = q.front();
 			q.pop();
 			for (size_t bitIndex = 0; bitIndex < N; ++bitIndex) {
 				if (arcFlag.GetBit(bitIndex) == true) continue;
@@ -69,10 +103,13 @@ namespace arcflags
 					arcFlagMap[startArcFlag] = findIt->second;
 					return;
 				}
-				q.push(arcFlag);
+				if (distance < maxDistance)
+					q.push(make_pair(arcFlag, distance + 1));
 				arcFlag.SetBit(bitIndex, false);
 			}
 		}
+
+		arcFlagMap[startArcFlag] = selectedArcFlags.FindClosest(startArcFlag);
 	}
 
 	template <size_t N, typename Graph, typename ArcFlagsMap>
@@ -84,40 +121,52 @@ namespace arcflags
 		using ArcFlagType = typename ArcFlagsMap::value_type;
 		using ArcFlagComparerType = ArcFlagComparer<ArcFlagType, N>;
 
-		map<ArcFlagType, uint32_t, ArcFlagComparerType> arcFlagCount;
-
+		unordered_map<ArcFlagType, uint32_t> arcFlagCount;
+		cout << "Counting arc-flags" << endl;
 		for (const auto& v : Range(vertices(graph))) {
 			for (const auto& e : Range(out_edges(v, graph))) {
 				++arcFlagCount[get(arcflags, e)];
 			}
 		}
 
+		cout << "Total : " << arcFlagCount.size() << endl;
+		cout << "Sorting arc-flags" << endl;
 		vector<pair<uint32_t, ArcFlagType>> countedArcFlags;
+		vector<ArcFlagType> allArcFlags;
 		countedArcFlags.reserve(arcFlagCount.size());
+		allArcFlags.reserve(arcFlagCount.size());
 		for (auto& kv : arcFlagCount) {
 			countedArcFlags.push_back(make_pair(kv.second, kv.first));
+			allArcFlags.push_back(kv.first);
 		}
 		auto arcFlagComparer = ArcFlagComparerType();
 		stable_sort(countedArcFlags.begin(), countedArcFlags.end(),
 		            [&arcFlagComparer](const pair<uint32_t, ArcFlagType>& a, const pair<uint32_t, ArcFlagType>& b) {
 			            return a.first < b.first || (a.first == b.first && arcFlagComparer(a.second, b.second));
 		            });
+		stable_sort(allArcFlags.begin(), allArcFlags.end(), ArcFlagComparerType());
+		reverse(allArcFlags.begin(), allArcFlags.end());
 		auto targetSize = static_cast<size_t>(ceil(countedArcFlags.size() * (1.0 - filter)));
 		countedArcFlags.resize(targetSize);
+		cout << "Arc-flags left : " << countedArcFlags.size() << endl;
+		cout << "Creating mapping" << endl;
 
 		auto fullArcFlag = FullArcFlag<ArcFlagType, N>();
-		map<ArcFlagType, ArcFlagType, ArcFlagComparerType> arcFlagMap;
+		unordered_map<ArcFlagType, ArcFlagType> arcFlagMap;
+		ArcFlagStorage<ArcFlagType, N> selectedArcFlags;
 		arcFlagMap[fullArcFlag] = fullArcFlag;
+		selectedArcFlags.Add(fullArcFlag);
 		for (auto& pair : countedArcFlags) {
 			arcFlagMap[pair.second] = pair.second;
+			selectedArcFlags.Add(pair.second);
 		}
-		for (auto& kv : Range(arcFlagCount.rbegin(), arcFlagCount.rend())) {
-			auto arcFlag = kv.first;
+		for (auto& arcFlag : allArcFlags) {
 			if (arcFlagMap.find(arcFlag) != arcFlagMap.end())
 				continue;
-			FindClosestArcFlag<N>(arcFlag, arcFlagMap);
+			FindClosestArcFlag<N>(arcFlag, arcFlagMap, selectedArcFlags);
 		}
 
+		cout << "Mapping arc-flags" << endl;
 		for (const auto& v : Range(vertices(graph))) {
 			for (const auto& e : Range(out_edges(v, graph))) {
 				auto arcFlag = get(arcflags, e);
@@ -138,7 +187,7 @@ namespace arcflags
 		std::queue<pair<ArcFlag, uint8_t>> q;
 		q.push(make_pair(startArcFlag, 0));
 		double startFlagCount = arcFlagCount[startArcFlag];
-		int breakDistance = 3;
+		int breakDistance = 0;
 		while (!q.empty()) {
 			ArcFlag arcFlag;
 			uint8_t distance;
@@ -148,7 +197,7 @@ namespace arcflags
 				if (arcFlag.GetBit(bitIndex) == true) continue;
 				arcFlag.SetBit(bitIndex, true);
 				arcFlagCount[arcFlag] += startFlagCount * pow(fadeAlpha, distance + 1);
-				if(distance < breakDistance)
+				if (distance < breakDistance)
 					q.push(make_pair(arcFlag, distance + 1));
 				arcFlag.SetBit(bitIndex, false);
 			}
@@ -164,24 +213,35 @@ namespace arcflags
 		using ArcFlagType = typename ArcFlagsMap::value_type;
 		using ArcFlagComparerType = ArcFlagComparer<ArcFlagType, N>;
 
-		map<ArcFlagType, double, ArcFlagComparerType> arcFlagCount;
-
+		unordered_map<ArcFlagType, uint32_t> arcFlagCount;
+		cout << "Counting arc-flags" << endl;
 		for (const auto& v : Range(vertices(graph))) {
 			for (const auto& e : Range(out_edges(v, graph))) {
 				++arcFlagCount[get(arcflags, e)];
 			}
 		}
+
+		cout << "Total : " << arcFlagCount.size() << endl;
+		cout << "Propagating ranks" << endl;
+
 		auto targetSize = static_cast<size_t>(ceil(arcFlagCount.size() * (1.0 - filter)));
 
-		vector<pair<double, ArcFlagType>> countedArcFlags;
+		vector<pair<uint32_t, ArcFlagType>> countedArcFlags;
+		vector<ArcFlagType> allArcFlags;
 		countedArcFlags.reserve(arcFlagCount.size());
+		allArcFlags.reserve(arcFlagCount.size());
 		for (auto& kv : arcFlagCount) {
 			countedArcFlags.push_back(make_pair(kv.second, kv.first));
+			allArcFlags.push_back(kv.first);
 		}
+		stable_sort(allArcFlags.begin(), allArcFlags.end(), ArcFlagComparerType());
+		reverse(allArcFlags.begin(), allArcFlags.end());
 
-		for(auto& kv : countedArcFlags) {
+		for (auto& kv : countedArcFlags) {
 			PropagateRank<N>(kv.second, arcFlagCount);
 		}
+
+		cout << "Sorting arc-flags" << endl;
 
 		countedArcFlags.clear();
 		countedArcFlags.reserve(arcFlagCount.size());
@@ -190,24 +250,29 @@ namespace arcflags
 		}
 		auto arcFlagComparer = ArcFlagComparerType();
 		stable_sort(countedArcFlags.begin(), countedArcFlags.end(),
-			[&arcFlagComparer](const pair<double, ArcFlagType>& a, const pair<double, ArcFlagType>& b) {
-			return a.first < b.first || (a.first == b.first && arcFlagComparer(a.second, b.second));
-		});
+		            [&arcFlagComparer](const pair<double, ArcFlagType>& a, const pair<double, ArcFlagType>& b) {
+			            return a.first < b.first || (a.first == b.first && arcFlagComparer(a.second, b.second));
+		            });
 		countedArcFlags.resize(targetSize);
+		cout << "Arc-flags left : " << targetSize << endl;
+		cout << "Creating mapping" << endl;
 
 		auto fullArcFlag = FullArcFlag<ArcFlagType, N>();
-		map<ArcFlagType, ArcFlagType, ArcFlagComparerType> arcFlagMap;
+		unordered_map<ArcFlagType, ArcFlagType> arcFlagMap;
+		ArcFlagStorage<ArcFlagType, N> selectedArcFlags;
 		arcFlagMap[fullArcFlag] = fullArcFlag;
+		selectedArcFlags.Add(fullArcFlag);
 		for (auto& pair : countedArcFlags) {
 			arcFlagMap[pair.second] = pair.second;
+			selectedArcFlags.Add(pair.second);
 		}
-		for (auto& kv : Range(arcFlagCount.rbegin(), arcFlagCount.rend())) {
-			auto arcFlag = kv.first;
+		for (auto& arcFlag : allArcFlags) {
 			if (arcFlagMap.find(arcFlag) != arcFlagMap.end())
 				continue;
-			FindClosestArcFlag<N>(arcFlag, arcFlagMap);
+			FindClosestArcFlag<N>(arcFlag, arcFlagMap, selectedArcFlags);
 		}
 
+		cout << "Mapping arc-flags" << endl;
 		for (const auto& v : Range(vertices(graph))) {
 			for (const auto& e : Range(out_edges(v, graph))) {
 				auto arcFlag = get(arcflags, e);
