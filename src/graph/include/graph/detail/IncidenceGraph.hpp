15fd7cbce46024304dd1cbb15b609f5f07b9e5d6
msamoylenko <ghost1212@mail.ru>
2015-12-24 18:58:53 +0500

    IncidenceGraph adaptor.

---
+++ src/graph/include/graph/detail/IncidenceGraph.hpp
@@ -0,0 +1,174 @@
+#pragma once
+#include <graph/properties.hpp>
+
+namespace graph
+{
+	template <typename Graph, typename VertexProperties, typename EdgeProperties, typename EnableIf = void>
+	class IncidenceGraphObfuscated;
+
+	template <typename Graph, typename VertexProperties, typename EdgeProperties>
+	class IncidenceGraphObfuscated
+			<Graph, VertexProperties, EdgeProperties,
+			 std::enable_if_t<
+				 detail::IsGraph<Graph>::value &&
+				 std::is_same<VertexProperties, typename Graph::vertex_bundled>::value &&
+				 std::is_same<EdgeProperties, typename Graph::edge_bundled>::value
+			 >
+			> {
+	public:
+		using InnerGraphType = Graph;
+
+		using vertices_size_type = typename Graph::vertices_size_type;
+		using edges_size_type = typename Graph::edges_size_type;
+		using degree_size_type = typename Graph::degree_size_type;
+		using vertex_descriptor = typename Graph::vertex_descriptor;
+		using directed_category = typename Graph::directed_category;
+		using edge_parallel_category = typename Graph::edge_parallel_category;
+		using traversal_category = typename Graph::traversal_category;
+		using edge_descriptor = typename Graph::edge_descriptor;
+
+		using vertex_bundled = typename Graph::vertex_bundled;
+		using edge_bundled = typename Graph::edge_bundled;
+
+		using in_edge_iterator = typename Graph::out_edge_iterator;
+		using out_edge_iterator = typename Graph::out_edge_iterator;
+		using adjacency_iterator = typename Graph::adjacency_iterator;
+		using vertex_iterator = typename Graph::vertex_iterator;
+
+		static vertex_descriptor null_vertex() {
+			return Graph::null_vertex();
+		};
+
+		explicit IncidenceGraphObfuscated(Graph& graph)
+			: innerGraph(graph) {}
+
+		operator Graph&() {
+			return innerGraph;
+		}
+
+		Graph& innerGraph;
+	};
+
+	template <typename Graph>
+	using IncidenceGraph = IncidenceGraphObfuscated<Graph, typename Graph::vertex_bundled, typename Graph::edge_bundled>;
+
+	// PropertyMaps
+#define IncidenceGraphTemplate template<typename Graph, typename VertexProperties, typename EdgeProperties>
+#define IncidenceGraphType IncidenceGraphObfuscated<Graph, VertexProperties, EdgeProperties>
+
+	IncidenceGraphTemplate
+	struct property_map<IncidenceGraphType, vertex_bundle_t> {
+		using type = typename property_map<typename IncidenceGraphType::InnerGraphType, vertex_bundle_t>::type;
+	};
+
+	IncidenceGraphTemplate
+	struct property_map<IncidenceGraphType, edge_bundle_t> {
+		using type = typename property_map<typename IncidenceGraphType::InnerGraphType, edge_bundle_t>::type;
+	};
+
+	IncidenceGraphTemplate
+	struct property_map<IncidenceGraphType, vertex_index_t> {
+		using type = typename property_map<typename IncidenceGraphType::InnerGraphType, vertex_index_t>::type;
+	};
+
+	IncidenceGraphTemplate
+	inline typename property_map<IncidenceGraphType, vertex_bundle_t>::type
+	get(const vertex_bundle_t& bundle, IncidenceGraphType& graph) {
+		return get(bundle, graph.innerGraph);
+	}
+
+	IncidenceGraphTemplate
+	inline typename property_map<IncidenceGraphType, edge_bundle_t>::type
+	get(const edge_bundle_t& bundle, IncidenceGraphType& graph) {
+		return get(bundle, graph.innerGraph);
+	}
+
+	IncidenceGraphTemplate
+	inline typename property_map<IncidenceGraphType, vertex_index_t>::type
+	get(const vertex_index_t& bundle, IncidenceGraphType& graph) {
+		return get(bundle, graph.innerGraph);
+	}
+
+
+	// External functions
+
+	IncidenceGraphTemplate
+	inline std::pair<typename IncidenceGraphType::vertex_iterator, typename IncidenceGraphType::vertex_iterator>
+	vertices(const IncidenceGraphType& g) {
+		return vertices(g.innerGraph);
+	}
+
+	IncidenceGraphTemplate
+	inline std::pair<typename IncidenceGraphType::adjacency_iterator, typename IncidenceGraphType::adjacency_iterator>
+	adjacent_vertices(typename IncidenceGraphType::vertex_descriptor u, const IncidenceGraphType& g) {
+		return adjacent_vertices(u, g.innerGraph);
+	}
+
+	IncidenceGraphTemplate
+	inline std::pair<typename IncidenceGraphType::adjacency_iterator, typename IncidenceGraphType::adjacency_iterator>
+	in_adjacent_vertices(typename IncidenceGraphType::vertex_descriptor u, const IncidenceGraphType& g) {
+		return adjacent_vertices(u, g.innerGraph);
+	}
+
+	IncidenceGraphTemplate
+	inline typename IncidenceGraphType::vertex_descriptor source(
+		typename IncidenceGraphType::edge_descriptor e, const IncidenceGraphType& g) {
+		return target(e, g.innerGraph);
+	}
+
+	IncidenceGraphTemplate
+	inline typename IncidenceGraphType::vertex_descriptor target(
+		typename IncidenceGraphType::edge_descriptor e, const IncidenceGraphType& g) {
+		return source(e, g.innerGraph);
+	}
+
+	IncidenceGraphTemplate
+	inline std::pair<typename IncidenceGraphType::out_edge_iterator, typename IncidenceGraphType::out_edge_iterator>
+	out_edges(typename IncidenceGraphType::vertex_descriptor u, const IncidenceGraphType& g) {
+		return out_edges(u, g.innerGraph);
+	}
+
+	IncidenceGraphTemplate
+	inline std::pair<typename IncidenceGraphType::in_edge_iterator, typename IncidenceGraphType::in_edge_iterator>
+	in_edges(typename IncidenceGraphType::vertex_descriptor v, const IncidenceGraphType& g) {
+		return out_edges(v, g.innerGraph);
+	}
+
+	IncidenceGraphTemplate
+	inline typename IncidenceGraphType::degree_size_type out_degree(
+		typename IncidenceGraphType::vertex_descriptor u, const IncidenceGraphType& g) {
+		return out_degree(u, g.innerGraph);
+	}
+
+	IncidenceGraphTemplate
+	inline typename IncidenceGraphType::degree_size_type in_degree(
+		typename IncidenceGraphType::vertex_descriptor v, const IncidenceGraphType& g) {
+		return out_degree(v, g.innerGraph);
+	}
+
+	IncidenceGraphTemplate
+	inline typename IncidenceGraphType::degree_size_type degree(
+		typename IncidenceGraphType::vertex_descriptor v, const IncidenceGraphType& g) {
+		return out_degree(v, g.innerGraph);
+	}
+
+	IncidenceGraphTemplate
+	inline typename IncidenceGraphType::vertices_size_type num_vertices(const IncidenceGraphType& g) {
+		return num_vertices(g.innerGraph);
+	}
+
+	IncidenceGraphTemplate
+	inline typename IncidenceGraphType::edges_size_type num_edges(const IncidenceGraphType& g) {
+		return num_edges(g.innerGraph);
+	}
+
+	IncidenceGraphTemplate
+	inline std::pair<typename IncidenceGraphType::edge_descriptor, bool> edge(
+		typename IncidenceGraphType::vertex_descriptor u,
+		typename IncidenceGraphType::vertex_descriptor v,
+		const IncidenceGraphType& graph) {
+		return edge(v, u, graph.innerGraph);
+	}
+#undef IncidenceGraphType
+#undef IncidenceGraphTemplate
+}
---
+++ src/graph/test/other/GraphInterface_Tests.cpp
@@ -0,0 +1,132 @@
+#include <gtest/gtest.h>
+#include <graph/static_graph.hpp>
+#include <graph/detail/ComplementGraph.hpp>
+#include <graph/detail/IncidenceGraph.hpp>
+#include <graph/detail/util/Collection.hpp>
+#include <vector>
+#include <random>
+
+using namespace std;
+using namespace graph;
+using namespace graphUtil;
+
+struct SomeVertexPropertyType{};
+struct SomeEdgePropertyType{};
+using VertexProperties = Properties<Property<SomeVertexPropertyType, uint32_t>>;
+using EdgeProperties = Properties<Property<SomeEdgePropertyType, uint32_t>>;
+using Graph = StaticGraph<VertexProperties, EdgeProperties>;
+
+inline vector<pair<size_t, size_t>> GenerateRandomEdgeList(int verticesCount, int edgesCount) {
+	vector<pair<size_t, size_t>> possibleEdges;
+	for (size_t v : Range(0, verticesCount)) {
+		for (size_t to : Range(0, verticesCount)) {
+			if (v == to) continue;
+			possibleEdges.push_back(make_pair(v, to));
+		}
+	}
+
+	random_device random_device;
+	mt19937 generator(random_device());
+	shuffle(possibleEdges.begin(), possibleEdges.end(), generator);
+	possibleEdges.erase(possibleEdges.begin() + edgesCount, possibleEdges.end());
+	return possibleEdges;
+}
+
+template<typename Graph, bool IsBidirectional = true>
+inline void VerifyGraphStructure(Graph &g, int n, int m) {
+
+	EXPECT_EQ(num_vertices(g), n);
+	EXPECT_EQ(num_edges(g), m);
+
+	auto graphVertices = SortedValuesRange(vertices(g));
+	EXPECT_TRUE(graphVertices.contains(0));
+	EXPECT_FALSE(graphVertices.contains(n));
+
+	for (auto& v : graphVertices) {
+		auto inAdjacencies = Range(in_adjacent_vertices(v, g));
+		EXPECT_TRUE(std::is_sorted(inAdjacencies.begin(), inAdjacencies.end()));
+		auto outAdjacencies = Range(adjacent_vertices(v, g));
+		EXPECT_TRUE(std::is_sorted(outAdjacencies.begin(), outAdjacencies.end()));
+
+		EXPECT_EQ(Range(out_edges(v, g)).size(), out_degree(v, g));
+		for (const auto& e : Range(out_edges(v, g))) {
+			auto to = target(e, g);
+			auto toInEdges = ValuesRange(in_edges(to, g));
+			EXPECT_TRUE(toInEdges.contains(e));
+		}
+		
+		if (IsBidirectional) {
+			EXPECT_EQ(Range(in_edges(v, g)).size(), in_degree(v, g));
+			for (const auto& e : Range(in_edges(v, g))) {
+				auto to = source(e, g);
+				auto toOutEdges = ValuesRange(out_edges(to, g));
+				EXPECT_TRUE(toOutEdges.contains(e));
+			}
+		}
+	}
+}
+
+template<typename Graph>
+inline void VerifyPropertyMaps(Graph &g, int n, int m) {
+	auto vertexIndex = graph::get(vertex_index_t(), g);
+	auto someVertexProperty = graph::get(SomeVertexPropertyType(), g);
+	auto someEdgeProperty = graph::get(SomeEdgePropertyType(), g);
+
+	typename property_map<Graph, vertex_index_t>::type::value_type vertexId = 0;
+	for (const auto& v : Range(vertices(g))) {
+		put(someVertexProperty, v, get(vertexIndex, v));
+		EXPECT_EQ(vertexId, get(someVertexProperty, v));
+		++vertexId;
+
+		typename property_map<Graph, SomeEdgePropertyType>::type::value_type edgeId;
+		for (const auto& e : Range(out_edges(v, g))) {
+			put(someEdgeProperty, e, edgeId);
+			EXPECT_EQ(edgeId, get(someEdgeProperty, e));
+			++edgeId;
+		}
+	}
+}
+
+TEST(GraphInterface, GraphBuilderCorrectness) {
+	int n = 100;
+	int m = 1000;
+
+	auto edgeList = GenerateRandomEdgeList(n, m);
+	auto g = Graph(edgeList.begin(), edgeList.end(), n, m);
+
+	VerifyGraphStructure(g, n, m);
+	VerifyPropertyMaps(g, n, m);
+}
+
+TEST(GraphInterface, ComplementGraphValidation) {
+
+	using Graph = StaticGraph<VertexProperties, EdgeProperties>;
+	using CGraph = ComplementGraph<Graph>;
+
+	int n = 100;
+	int m = 1000;
+
+	auto edgeList = GenerateRandomEdgeList(n, m);
+	auto graph = Graph(edgeList.begin(), edgeList.end(), n, m);
+	auto cGraph = CGraph(graph);
+
+	VerifyGraphStructure(cGraph, n, m);
+	VerifyPropertyMaps(cGraph, n, m);
+}
+
+TEST(GraphInterface, IncidenceGraphValidation) {
+	using VertexProperties = Properties<Property<SomeVertexPropertyType, int>>;
+	using EdgeProperties = Properties<Property<SomeEdgePropertyType, int>>;
+	using Graph = StaticGraph<VertexProperties, EdgeProperties>;
+	using IGraph = IncidenceGraph<Graph>;
+
+	int n = 100;
+	int m = 1000;
+
+	auto edgeList = GenerateRandomEdgeList(n, m);
+	auto graph = Graph(edgeList.begin(), edgeList.end(), n, m);
+	auto iGraph = IGraph(graph);
+
+	VerifyGraphStructure<IGraph, false>(iGraph, n, m);
+	VerifyPropertyMaps(iGraph, n, m);
+}
